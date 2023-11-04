65419b4116fe1c36fd735386d90af9ba74279d02
kacper-o <kacper093@icloud.com>
2023-11-04 11:22:29 +0100

    filters completed

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,8 @@ import Image from 'next/image'
 
 import { CarProps } from '@/types'
 import  CustomButton  from './CustomButton'
-import { calculateCarRent } from '@/utils'
+import { calculateCarRent, generateCarImageUrl } from '@/utils'
+import CarDetails from "./CarDetails"
 
 interface CarCardProps {
     car: CarProps
@@ -33,7 +34,7 @@ const CarCard = ({ car } : CarCardProps) => {
         </span>
       </p>
       <div className='relative w-full h-40 my-3 object-contain'>
-        <Image src="/hero.png" alt="car model" fill priority className='object-contain'/>
+        <Image src={generateCarImageUrl(car)} alt="car model" fill priority className='object-contain'/>
       </div>
       <div className='relative flex w-full mt-2'>
         <div className='flex group-hover:invisible w-full justify-between text-gray'>
@@ -60,6 +61,7 @@ const CarCard = ({ car } : CarCardProps) => {
           <CustomButton title="View more" containerStyles='w-full py-[16px] rounded-full bg-primary-blue' textStyles="text-white text-[14px] leading-[17px] font-bold" rightIcon="/right-arrow.svg" handleClick={() => setIsOpen(true)}/>
         </div>
       </div>
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
     </div>
   )
 }
